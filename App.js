e7300f45418c2dcdd965e435a467f0bf8fb553a0
Diego Sousa <derickbenji2@gmail.com>
2020-09-07 16:58:47 -0300

    iniciando projeto

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
+import image from './src/assets/bg.jpg'
 export default function App() {
+
+  //const image = {uri:''};
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     
+        <ImageBackground source={image} style={styles.image}>
+          <View style={styles.coverView}>
+            <Text style={styles.textHeader}>Lista de Tarefas - Diego Dev</Text>
+          </View>
+        </ImageBackground>
+     
+
+      <StatusBar hidden/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex:1
   },
+
+  image:{
+    width: '100%',
+    height: 80,
+    resizeMode: 'cover',
+ 
+  },
+
+  textHeader:{
+    textAlign:'center',
+    color: '#fff',
+    fontSize:20,
+    marginTop: 20,
+  },
+
+  coverView:{
+    width:'100%',
+    height:80,
+    backgroundColor: 'rgba(0,0,0,0.3)'
+  }
 });
